d89e228e84208e7ed53a236a2dd4bd1ed8eca21d
Igor Popikov <titbm@yandex.ru>
2016-11-03 23:38:20 +0300

    Refactor with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var server = express();
 const PORT = process.env.PORT || 3000;
@@ -18,13 +19,7 @@ server.get('/todos', function(request, response) {
 // GET /todos/:id
 server.get('/todos/:id', function(request, response) {
   var todoId = parseInt(request.params.id, 10); // request.params - объект с данными, которые передаются в запросе через двоеточие
-  var matchedTodo = null;
-
-  todos.forEach(function(todo) {
-    if (todo.id === todoId) {
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, { id: todoId }); // underscore-функция для поиска модели в коллекции todos элемента с id = totoId
 
   if (matchedTodo) {
     response.status(200).json(matchedTodo); // status(200) - отправить страницу со статусом 200 и данные в JSON-формате. status(200) - указывать необязательно
@@ -38,11 +33,16 @@ server.get('/todos/:id', function(request, response) {
 // Такой метод безопанее, чем GET-запрос и позволяет передавать сложные данные, такие как файл или JSON (данный случай)
 
 server.post('/todos', function(request, response){
-  var body = request.body; // в request.body содержаться данные обработанные body-parser.json()
+  var body = _.pick(request.body, 'description', 'completed'); // .pick - фильтрует свойства в объекте (чтобы не передавали лишнее), в request.body содержаться данные обработанные body-parser.json()
 
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return response.status(404).send();
+  }
+
+  body.description = body.description.trim();
   body.id = todoNextId++;
   todos.push(body);
-  
+
   response.json(body);
 });
 
