94982c509cf412384171b3ecbd5d4f411a40b0ff
Igor Popikov <titbm@yandex.ru>
2016-11-03 22:51:35 +0300

    Add body-parser & POST for todos

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var server = express();
 const PORT = process.env.PORT || 3000;
 
-var todos =[ // Коллекция данных
-  {
-    id: 1, // модель данных
-    description: 'Meet mom for lunch',
-    completed: false
-  },
-  {
-    id: 2, // модель данных
-    description: 'Go to market',
-    completed: false
-  },
-  {
-    id: 3, // модель данных
-    description: 'Feed the cat',
-    completed: true
-  }
-];
+var todos = []; // коллекция данных
+var todoNextId = 1;
 
 
-// GET /todos
+server.use(bodyParser.json()); // middleware - для обработки данных, присланных в формате JSON
+
+// GET /todos - GET-запросы используются для получения данных
 server.get('/todos', function(request, response) {
   response.json(todos); // response.json - метод Express для отправки объектов в формате json
 });
@@ -31,7 +20,6 @@ server.get('/todos/:id', function(request, response) {
   var todoId = parseInt(request.params.id, 10); // request.params - объект с данными, которые передаются в запросе через двоеточие
   var matchedTodo = null;
 
-  // response.send(todoId);
   todos.forEach(function(todo) {
     if (todo.id === todoId) {
       matchedTodo = todo;
@@ -43,7 +31,19 @@ server.get('/todos/:id', function(request, response) {
   } else {
     response.status(404).send(); // status(404) - отправить страницу со статусом 404, если адреса не существует
   }
+});
 
+// POST /todos - POST-запросы используются для передачи данных.
+// При выполнении POST-запроса данные записываются в заголовок запроса, а не в URL.
+// Такой метод безопанее, чем GET-запрос и позволяет передавать сложные данные, такие как файл или JSON (данный случай)
+
+server.post('/todos', function(request, response){
+  var body = request.body; // в request.body содержаться данные обработанные body-parser.json()
+
+  body.id = todoNextId++;
+  todos.push(body);
+  
+  response.json(body);
 });
 
 server.get('/', function(request, response){
