bd14f3935bd8c066f52aad3055d873acd3b89d59
Igor Popikov <titbm@yandex.ru>
2016-11-04 19:27:41 +0300

    Add Postgress for Production

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 var sequelize = require('sequelize');
-var dataBaseManager = new sequelize(undefined, undefined, undefined, {
-  'dialect': 'sqlite', // указание на то, какой типы базы данных будет использован
-  'storage': __dirname + '/data/dev-todo-api.sqlite' // файл, в котором будет база данных
-});
+var env = process.env.NODE_ENV || 'development';
+
+var dataBaseManager = null;
+
+if (env === 'production') {
+  var dataBaseManager = new sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+  });
+} else {
+  var dataBaseManager = new sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite', // указание на то, какой типы базы данных будет использован
+    'storage': __dirname + '/data/dev-todo-api.sqlite' // файл, в котором будет база данных
+  });
+}
 
 var db = {}; // create database...
 
